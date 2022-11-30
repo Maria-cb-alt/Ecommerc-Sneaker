4276d90ddd19d36d9f49e79583c0ddec727f7112
Carol <carolzinhabalassoni@gmail.com>
2022-11-29 23:25:08 -0300

    a

---
+++ components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2022 Alex Headphones Todos os direitos reservados</p>
+      <p className="icons">
+        <a href="https://www.instagram.com/alex.maceedo/" target="_blank">
+          <AiFillInstagram />
+        </a>
+        <a href="https://www.linkedin.com/in/alex-macedo-610860235/" target="_blank">
+          <AiFillLinkedin />
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default Footer
+
